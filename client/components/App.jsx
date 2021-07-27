3883276b43a03a4cff1fd29a1aedcf3dc3322e85
Melanie DeWitt <melaniebd1986@gmail.com>
2021-07-26 18:44:52 -0600

    tab functionality added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,19 +1,64 @@
 import React from 'react';
+import { Tab, Tabs, AppBar } from '@material-ui/core';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      tab: 0
+    }
+    this.handleTabs = this.handleTabs.bind(this);
+    this.Tab1 = this.Tab1.bind(this);
   };
 
   componentDidMount() {
 
   }
 
+  handleTabs(e, value) {
+    console.log(value);
+    this.setState({
+      tab: value
+    });
+  }
+
+  Tab1() {
+    if (this.state.tab === 0) {
+      return(<h1>Tab 1</h1>);
+    } else {
+      return(<div></div>)
+    }
+  }
+
+  Tab2() {
+    if (this.state.tab === 1) {
+      return(<h1>Tab 2</h1>);
+    } else {
+      return(<div></div>)
+    }
+  }
+
+  Tab3() {
+    if (this.state.tab === 2) {
+      return(<h1>Tab 3</h1>);
+    } else {
+      return(<div></div>)
+    }
+  }
+
+
+
   render() {
     return(
       <div>
-        Hello World
+        <h1>TTRPG Meets Computer</h1>
+        <AppBar position='static' >
+          <Tabs value={this.state.tab} onChange={this.handleTabs}>
+            <Tab label='Character'/>
+            <Tab label='Encounter'/>
+            <Tab label='Miscellaneous'/>
+          </Tabs>
+        </AppBar>
       </div>
     );
   }
---
+++ client/components/Character.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Character = (props) => {
+  // do stuff and things here
+
+}
+
+export default Character;

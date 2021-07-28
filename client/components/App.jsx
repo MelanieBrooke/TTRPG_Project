04f4edd6764bcfd20f813df55a7a7f3d720e7100
Melanie DeWitt <melaniebd1986@gmail.com>
2021-07-27 19:54:45 -0600

    character creation button and popup added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,15 +3,18 @@ import { Tab, Tabs, AppBar } from '@material-ui/core';
 import Character from './Character.jsx';
 import Encounters from './Encounters.jsx';
 import Miscellaneous from './Miscellaneous.jsx';
+import Creation from './Creation.jsx';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tab: 0
+      tab: 0,
+      creationPopup: false
     }
     this.handleTabs = this.handleTabs.bind(this);
+    this.toggleCreationPopup = this.toggleCreationPopup.bind(this);
   };
 
   componentDidMount() {
@@ -19,9 +22,14 @@ class App extends React.Component {
   }
 
   handleTabs(e, value) {
-    // console.log(value);
     this.setState({
       tab: value
+    });
+  }
+
+  toggleCreationPopup() {
+    this.setState({
+      creationPopup: !this.state.creationPopup
     });
   }
 
@@ -32,14 +40,20 @@ class App extends React.Component {
         <h1>TTRPG Meets Computer</h1>
         <AppBar position='static' >
           <Tabs value={this.state.tab} onChange={this.handleTabs}>
-            <Tab label='Character'/>
+            <Tab label='Character Sheet'/>
             <Tab label='Encounter'/>
             <Tab label='Miscellaneous'/>
           </Tabs>
         </AppBar>
 {/* Spaced out for clarity while coding */}
+        <Creation
+        creationPopup={this.state.creationPopup}
+        toggleCreationPopup={this.toggleCreationPopup}
+        />
+
         <Character
         tab={this.state.tab}
+        toggleCreationPopup={this.toggleCreationPopup}
         />
 
         <Encounters
--- client/components/Character.jsx
+++ client/components/Character.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Character = (props) => {
   // do stuff and things here
   if (props.tab === 0) {
-    return (<h1>Character Page Under Construction</h1>)
+    return (<div>
+              <br></br>
+              <br></br>
+              <button onClick={props.toggleCreationPopup}>Character Creation</button>
+              <br></br>
+              <br></br>
+              <h1>Character Page Under Construction</h1>
+            </div>)
   } else {
     return (<div></div>);
   }

5004f6a8e4f12fe60195dd4fc948260f202864b6
Melanie DeWitt <melaniebd1986@gmail.com>
2021-07-30 11:59:11 -0600

    setting up races menu

--- client/components/Creation.jsx
+++ client/components/Creation.jsx
@@ -10,6 +10,65 @@ const Creation = (props) => {
                   <span class={creationpopup.close} onClick={props.toggleCreationPopup}>&times;</span>
                   <br></br>
                   <h1>This is where the character creation magic will happen</h1>
+                  <br></br>
+                  <form>
+                    <label for="race">Choose a Race:</label>
+                    <select name="race" id="race">
+                      <optgroup label="Human">
+                        <option value="hardy">Hardy and Strong</option>
+                        <option value="small">Small and Slender</option>
+                        <option value="middle">Average</option>
+                      </optgroup>
+                      <optgroup label="Elf">
+                        <option value="winged">Winged Elf</option>
+                        <option value="red">Red Elf</option>
+                        <option value="mystic">Mystic Elf</option>
+                      </optgroup>
+                      <optgroup label="Elemental">
+                        <option value="fire">Fire Elemental</option>
+                        <option value="ice">Ice Elemental</option>
+                        <option value="earth">Earth Elemental</option>
+                        <option value="storm">Storm Elemental</option>
+                      </optgroup>
+                      <optgroup label="Undead">
+                        <option value="vampire">Vampire</option>
+                        <option value="ghoul">Ghoul</option>
+                        <option value="skeleton">Skeleton</option>
+                      </optgroup>
+                      <optgroup label="Elf">
+                        <option value="winged">Winged Elf</option>
+                        <option value="red">Red Elf</option>
+                        <option value="mystic">Mystic Elf</option>
+                      </optgroup>
+                      <optgroup label="Elf">
+                        <option value="winged">Winged Elf</option>
+                        <option value="red">Red Elf</option>
+                        <option value="mystic">Mystic Elf</option>
+                      </optgroup>
+                      <optgroup label="Elf">
+                        <option value="winged">Winged Elf</option>
+                        <option value="red">Red Elf</option>
+                        <option value="mystic">Mystic Elf</option>
+                      </optgroup>
+                      <optgroup label="Elf">
+                        <option value="winged">Winged Elf</option>
+                        <option value="red">Red Elf</option>
+                        <option value="mystic">Mystic Elf</option>
+                      </optgroup>
+                      <optgroup label="Elf">
+                        <option value="winged">Winged Elf</option>
+                        <option value="red">Red Elf</option>
+                        <option value="mystic">Mystic Elf</option>
+                      </optgroup>
+                      <optgroup label="Elf">
+                        <option value="winged">Winged Elf</option>
+                        <option value="red">Red Elf</option>
+                        <option value="mystic">Mystic Elf</option>
+                      </optgroup>
+                    </select>
+                    <br></br>
+                    <input type="submit" value="Submit"></input>
+                  </form>
                 </div>
               </div>
             </div>)
